aa4491172b2e437866cb1b637ab9dc84b2ad2f9f
Severin <Severin@158.37.228.40>
2022-09-29 10:18:40 +0200

    torsdag

--- src/no/hvl/dat100/lab5/tabeller/Tabeller.java
+++ src/no/hvl/dat100/lab5/tabeller/Tabeller.java
@@ -6,74 +6,56 @@ public class Tabeller {
 	public static void skrivUt(int[] tabell) {
 
 		// TODO A
-		
+
 		int i = 0;
 		int tall = 0;
 		String tallTxt = "Tabell: ";
 
 		for (i = 0; i < tabell.length; i++) {
 			tall = tabell[i];
-			tallTxt += tall + " ";	
-		}	
-		System.out.println(tallTxt);
-<<<<<<< HEAD
-		
+			tallTxt += tall + " ";
 		}
-		
-=======
-			
+		System.out.println(tallTxt);
+
 	}
->>>>>>> branch 'master' of https://github.com/s669799/dat100-javainnlevering2.git
 
 	// b)
 	public static String tilStreng(int[] tabell) {
 
 		// TODO Dag Åsmund
-<<<<<<< HEAD
 		System.out.println();
 		String s = "[" + tabell[0];
-=======
->>>>>>> branch 'master' of https://github.com/s669799/dat100-javainnlevering2.git
-		
-<<<<<<< HEAD
+
 		for (int i = 1; i < tabell.length; i++) {
 			s += ", " + tabell[i];
-			
-			}
+
+		}
 		s += "]" + "\n";
 		System.out.print(s);
 		return s;
-		}
-=======
-		System.out.println("[" + tabell[0] + ", " + tabell[1] + ", " + tabell[2] + "]");
-		
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
->>>>>>> branch 'master' of https://github.com/s669799/dat100-javainnlevering2.git
-		
-	
+	}
 
 	// c)
 	public static int summer(int[] tabell) {
-		// TODO Mateusz 
+		// TODO Mateusz
 		int sum = 0;
-		for(int i = 0; i < tabell.length; i++) {
+		for (int i = 0; i < tabell.length; i++) {
 			int b = tabell[i];
 			sum += b;
 		}
 		System.out.println(sum);
-		
+
 		int i = 0;
 		sum = 0;
-		while(i < tabell.length) {
+		while (i < tabell.length) {
 			int b = tabell[i];
 			sum += b;
 			i++;
 		}
 		System.out.println(sum);
-		
-		
+
 		sum = 0;
-		for(int k : tabell) {
+		for (int k : tabell) {
 			sum += k;
 		}
 		System.out.println(sum);
@@ -93,7 +75,7 @@ public class Tabeller {
 			}
 			i++;
 		}
-		
+
 		System.out.println("Finnes: " + finnes);
 		return finnes;
 
@@ -106,20 +88,11 @@ public class Tabeller {
 		int pos = -1;
 		for (int i = 0; i < tabell.length; i++) {
 			if (tabell[i] == tall)
-<<<<<<< HEAD
 				pos = i;
 		}
 		System.out.print("Posisjon: " + pos);
 		System.out.println();
 		return pos;
-=======
-				funnet = true;
-			else
-				i++;
-		}
-		return tall;
-
->>>>>>> branch 'master' of https://github.com/s669799/dat100-javainnlevering2.git
 	}
 
 	// f)
@@ -128,15 +101,15 @@ public class Tabeller {
 		// TODO Mateusz
 		int[] nytabell = new int[tabell.length];
 		int j = tabell.length;
-		for(int i = 0; i< tabell.length; i++) {
-			nytabell[j-1] = tabell[i];
+		for (int i = 0; i < tabell.length; i++) {
+			nytabell[j - 1] = tabell[i];
 			j--;
 		}
-		for(int i = 0; i< nytabell.length; i++)
+		for (int i = 0; i < nytabell.length; i++)
 			System.out.print(nytabell[i] + " ");
-		
+
 		return nytabell;
-		
+
 	}
 
 	// g)
@@ -145,18 +118,18 @@ public class Tabeller {
 		// TODO Severin
 		boolean sortert = true;
 		int i = 0;
-		
+
 		while (sortert && i < tabell.length - 1) {
-			if (tabell[i] <= tabell[i+1]) {
-				i++;		
+			if (tabell[i] <= tabell[i + 1]) {
+				i++;
 			} else {
 				sortert = false;
-			}			
+			}
 		}
-		
+
 		System.out.println("Sortert: " + sortert);
 		return sortert;
-		
+
 	}
 
 	// h)
